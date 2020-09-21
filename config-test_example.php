6b13531a6bd376ba4d4999cebdbd885d178440f4
Céline Pervès <cperves@unistra.fr>
2020-09-21 16:19:37 +0200

    [#11] unit test config file and explanations

---
+++ config-test_example.php
@@ -0,0 +1,7 @@
+<?php
+set_config('instanceurl','https://rocketchat-server_url','mod_rocketchat');
+set_config('restapiroot','/api/v1/','mod_rocketchat');
+set_config('apiuser','your_user_on_rocket.chat','mod_rocketchat');
+set_config('apipassword','#############','mod_rocketchat');
+// fake config test to avoird email domain troubles
+set_config('domainmail','your_domain_mail_if_necessary','mod_rocketchat'); // Optional argument.line.
