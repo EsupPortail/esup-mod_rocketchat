ad59bebae823f5de2013cb6e31eba93a0b4a8826
Céline Pervès <cperves@unistra.fr>
2020-12-15 15:05:27 +0100

    version.php corrections

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_rocketchat';
-$plugin->release = '1.0.0';
+$plugin->release = '1.2.1';
 $plugin->version = 2020120900;
 $plugin->requires = 2018050800;
-$plugin->maturity = MATURITY_RC;
+$plugin->maturity = MATURITY_STABLE;
