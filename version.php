8a2071b33eda0b46c6004af0df6b92e7ee5331d6
Céline Pervès <cperves@unistra.fr>
2021-06-14 11:23:21 +0200

    add version to push to moodle.org

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_rocketchat';
-$plugin->release = '1.2.9';
-$plugin->version = 2021022400;
+$plugin->release = '1.3.0';
+$plugin->version = 2021061400;
 $plugin->requires = 2018050800;
 $plugin->maturity = MATURITY_STABLE;
