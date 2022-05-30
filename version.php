2702060215ce1a3cbae7d5080967db4a8b95b8d8
Céline Pervès <cperves@unistra.fr>
2022-05-30 09:22:49 +0200

    prepare to publish

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_rocketchat';
-$plugin->release = '1.4.0';
-$plugin->version = 2022042900;
+$plugin->release = '1.4.1';
+$plugin->version = 2022053000;
 $plugin->requires = 2018050800;
 $plugin->maturity = MATURITY_STABLE;
