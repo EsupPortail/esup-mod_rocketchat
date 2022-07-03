f224efada1957aba22af68126e0cfa2d140700d9
Céline Pervès <cperves@unistra.fr>
2022-07-03 17:56:39 +0200

    remove bin directory in zip file

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_rocketchat';
-$plugin->release = '1.4.4';
+$plugin->release = '1.4.5'
 $plugin->version = 2022061601;
 $plugin->requires = 2018050800;
 $plugin->maturity = MATURITY_STABLE;
